6a71cdda0365b67a65d7bb9d08b156941b722353
Mehmet Efe Erbaş <103934031+meferbas@users.noreply.github.com>
2023-12-14 00:33:27 +0300

    Add files via upload

--- Controllers/FlightController.cs
+++ Controllers/FlightController.cs
@@ -5,6 +5,7 @@ using AirlineSeatReservationSystem.Entity;
 using AirlineSeatReservationSystem.Data.Abstract;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 
 namespace AirlineSeatReservationSystem.Controllers;
 
@@ -13,79 +14,194 @@ public class FlightController : Controller
     private IFlightRepository _repository;
     public FlightController(IFlightRepository repository)
     {
-        _repository=repository;
+        _repository = repository;
     }
     public IActionResult Index()
     {
         return View();
-    } 
+    }
 
     [HttpPost]
-        public async Task<IActionResult> Index(FlightViewModel model)
+    public async Task<IActionResult> Index(FlightViewModel model)
+    {
+        if (ModelState.IsValid)
         {
-            if (ModelState.IsValid)
+            var user = await _repository.Flights.FirstOrDefaultAsync(x => x.From == model.From && x.To == model.To);
+            if (user == null)
             {
-                var user = await _repository.Flights.FirstOrDefaultAsync(x=> x.From==model.From && x.To==model.To);
-                if (user == null)
+                _repository.getFlight(new Flight
                 {
-                    _repository.getFlight(new Flight
-                    {
-                        From=model.From,
-                        To=model.To,
-                        Depart=model.Depart,
-                        Return=model.Return,
-                        Guest=model.Guest
+                    From = model.From,
+                    To = model.To,
+                    Depart = model.Depart,
+                    Return = model.Return,
+                    Guest = model.Guest
 
-                    });
+                });
 
-                    return RedirectToAction("SignIn");
+                return RedirectToAction("SignIn");
+            }
+
+
+        }
+        return View(model);
+    }
+
+    [Authorize(Roles = "admin")]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [Authorize(Roles = "admin")]
+
+    public IActionResult Create(FlightCreateViewModel model)
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        if (ModelState.IsValid)
+        {
+            _repository.getFlight(
+                new Flight
+                {
+                    From = model.From,
+                    To = model.To,
+                    Depart = Convert.ToString(model.Depart),
+                    Return = model.Return,
+                    Time = model.Time,
+                    Guest = model.Guest,
+
                 }
-                
-
-            }
-            return View(model);
+            );
+            return RedirectToAction("ListFlights");
         }
+        return View();
+    }
 
 
-        public IActionResult Create()
+    public async Task<IActionResult> SearchFlights(FlightViewModel model)
+    {
+
+        if (ModelState.IsValid)
         {
-            return View();
+            var errors = ModelState.Values.SelectMany(v => v.Errors);
+            var flights = await _repository.Flights
+                .Where(f => f.From == model.From && f.To == model.To
+                            && f.Depart == model.Depart && f.Return == model.Return
+                            && f.Guest == model.Guest)
+                .Select(flightEntity => new FlightCreateViewModel
+                {
+
+                    From = flightEntity.From,
+                    To = flightEntity.To,
+                    Depart = flightEntity.Depart,
+                    Return = flightEntity.Return,
+                    Time = flightEntity.Time,
+
+                    Guest = flightEntity.Guest,
+
+                })
+                .ToListAsync();
+
+            return View("SearchResults", flights);
         }
 
-        [HttpPost]
-        public IActionResult Create(FlightCreateViewModel model)
-        {   
-            var userId=User.FindFirstValue(ClaimTypes.NameIdentifier);
-            if(ModelState.IsValid)
+        return View("Index", model);
+    }
+
+
+
+
+    [Authorize(Roles = "admin")]
+    public async Task<IActionResult> ListFlights()
+    {
+        var flights = await _repository.Flights
+            .Select(flightEntity => new FlightCreateViewModel
             {
-                _repository.getFlight(
-                    new Flight{
-                        From=model.From,
-                        To=model.To,
-                        Depart=model.Depart,
-                        Return=model.Return,
-                        Time=model.Time,
-                        Guest=model.Guest,
-            
-                    }
-                );
-                return RedirectToAction("Flight");
-            }
-            return View();
-        }
+                FlightId = flightEntity.FlightId,
+                From = flightEntity.From,
+                To = flightEntity.To,
+                Time = flightEntity.Time
+                // Burada diğer gerekli alanları da ekleyebilirsiniz
+            })
+            .ToListAsync();
 
-        
-        public async Task<IActionResult> List()
+        return View("SearchResults", flights);
+    }
+
+    [Authorize(Roles = "admin")]
+    public IActionResult Edit(int? id)
+    {
+        if (id == null)
         {
-            var flightId=int.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier) ?? "");
-            var role=User.FindFirstValue(ClaimTypes.Role);
-            var flight=_repository.Flights;
-            
-            if(string.IsNullOrEmpty(role))
-            {
-                flight=flight.Where(i=>i.FLightId==flightId);
-            }
-
-            return View(await flight.ToListAsync());
+            return NotFound();
         }
+        var ucus = _repository.Flights.FirstOrDefault(i => i.FlightId == id);
+        if (ucus == null)
+        {
+            return NotFound();
+        }
+
+
+        return View(new FlightCreateViewModel
+        {
+            FlightId = ucus.FlightId,
+            From = ucus.From,
+            To = ucus.To,
+            Depart = ucus.Depart,
+            Return = ucus.Return,
+            Time = ucus.Time,
+            Guest = ucus.Guest
+
+
+        });
+    }
+
+
+    [Authorize(Roles = "admin")]
+    [HttpPost]
+    public IActionResult Edit(FlightCreateViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var entityToUpdate = new Flight
+            {
+                FlightId = model.FlightId,
+                From = model.From,
+                To = model.To,
+                Depart = model.Depart,
+                Return = model.Return,
+                Time = model.Time,
+                Guest = model.Guest
+
+            };
+            _repository.editFlight(entityToUpdate);
+            return RedirectToAction("ListFlights");
+        }
+        return View();
+    }
+
+
+
+    [Authorize(Roles = "admin")]
+    [HttpPost]
+    public async Task<IActionResult> Delete(int? flightId)
+    {
+        if (flightId == null)
+        {
+            return NotFound(); // Uçuş ID'si sağlanmadıysa, 404 hatası döndür
+        }
+
+        var flight = await _repository.Flights.FirstOrDefaultAsync(f => f.FlightId == flightId.Value);
+        if (flight == null)
+        {
+            return NotFound(); // Uçuş bulunamadıysa, 404 hatası döndür
+        }
+
+        await _repository.DeleteFlight(flight); // DeleteFlight metodunu asenkron hale getirin
+        return RedirectToAction("ListFlights"); // Uçuş silindikten sonra, uçuş listesine yönlendir
+    }
+
+
+
 }
--- Data/Concrete/Efcore/EfFlightRepository.cs
+++ Data/Concrete/Efcore/EfFlightRepository.cs
@@ -13,11 +13,34 @@ namespace AirlineSeatReservationSystem.Data.Concrete
             _context = context;
         }
         public IQueryable<Flight> Flights => _context.Flights;
+
+        public async Task DeleteFlight(Flight flight)
+        {
+            _context.Flights.Remove(flight);
+            await _context.SaveChangesAsync(); // Asenkron kaydetme
+        }
+
+
+        public void editFlight(Flight Flight)
+        {
+            var entity = _context.Flights.FirstOrDefault(i => i.FlightId == Flight.FlightId);
+            if (entity != null)
+            {
+                entity.From = Flight.From;
+                entity.To = Flight.To;
+                entity.Depart = Flight.Depart;
+                entity.Return = Flight.Return;
+                entity.Time = Flight.Time;
+                entity.Guest = Flight.Guest;
+                _context.SaveChanges();
+            }
+        }
+
         public void getFlight(Flight Flight)
         {
             _context.Flights.Add(Flight);
             _context.SaveChanges();
         }
-        
+
     }
 }
